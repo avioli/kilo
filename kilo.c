19a6bb17b283f2472f79c39523f2935a47a18e07
Evo Stamatov <aviolito@gmail.com>
2017-04-09 22:39:22 +1000

    single-line if-blocks

--- kilo.c
+++ kilo.c
@@ -22,13 +22,11 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-		die("tcsetattr");
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
 }
 
 void enableRawMode() {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
-		die("tcgetattr");
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
@@ -39,8 +37,7 @@ void enableRawMode() {
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
 
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
-		die("tcsetattr");
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 /*** init ***/
@@ -50,10 +47,8 @@ int main() {
 
 	while (1) {
 		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-			die("read");
-		if (c == CTRL_KEY('q'))
-			break;
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+		if (c == CTRL_KEY('q')) break;
 	}
 
 	return 0;
