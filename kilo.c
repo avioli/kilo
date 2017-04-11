8ace4aa2473b6591650af9ffbfcc3f359d155932
Evo Stamatov <aviolito@gmail.com>
2017-04-12 00:28:21 +1000

    add getline feature test macro

--- kilo.c
+++ kilo.c
@@ -2,12 +2,18 @@
 // http://viewsourcecode.org/snaptoken/kilo/01.setup.html
 
 /*** includes ***/
+
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU SORUCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
